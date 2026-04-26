81944cdce18a8815905a6eb8bbdbcd53331be9ab
Varun E <var3907@gmail.com>
2026-04-26 23:14:33 +0530

    Delete legal_qa directory

--- legal_qa/evaluation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Evaluation modules for Legal QA.
-"""
-
-from legal_qa.evaluation.evaluator import QAEvaluator
-
-__all__ = ["QAEvaluator"]
--- legal_qa/inference/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Legal QA inference pipeline package.
-"""
-
-from legal_qa.inference.pipeline import LegalQAPipeline
-
-__all__ = ["LegalQAPipeline"]
